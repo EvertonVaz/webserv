bb7237c59fc26d1dc198ad4b66df3f555bbb0c00
Everton <egeraldo@student.42sp.org.br>
2024-10-03 12:05:02 -0300

    testando possibilidades pra fazer o parser 2

--- ConfigParser.cpp
+++ ConfigParser.cpp
@@ -6,7 +6,7 @@
 /*   By: Everton <egeraldo@student.42sp.org.br>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 14:10:43 by Everton           #+#    #+#             */
-/*   Updated: 2024/10/02 15:05:32 by Everton          ###   ########.fr       */
+/*   Updated: 2024/10/03 12:04:09 by Everton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,50 +20,73 @@ std::string ConfigParser::getFilename() const {
 	return filename;
 }
 
-bool ConfigParser::parse(std::vector<ServerConfig>& servers) {
-	std::ifstream file(filename.c_str());
-	if (!file.is_open()) {
-		std::cerr << "Failed to open file: " << filename << std::endl;
+bool newServerBlock(bool inServerBlock, ServerConfig& currentServer) {
+	if (inServerBlock) {
+		std::cerr << "Nested server blocks are not allowed." << std::endl;
 		return false;
 	}
-
-	std::string line;
-	ServerConfig currentServer;
-	RouteConfig currentRoute;
-	bool inServerBlock = false;
-	bool inLocationBlock = false;
-
-	while (std::getline(file, line)) {
-		std::istringstream iss(line);
-		std::string key;
-		std::string path;
-		iss >> key;
-
-		if (key == "server") {
-			currentServer = ServerConfig();
-			inServerBlock = true;
-		} else if (key == "location") {
-			currentRoute = RouteConfig();
-			inLocationBlock = true;
-			iss >> path;
-			currentRoute.path = path;
-		} else if (key == "}") {
-			if (inLocationBlock) {
-				currentServer.routes.push_back(currentRoute);
-				inLocationBlock = false;
-			} else if (inServerBlock) {
-				servers.push_back(currentServer);
-				inServerBlock = false;
-			}
-		} if (inServerBlock) {
-			parseServerDirective(currentServer, key, iss);
-		} if (inLocationBlock) {
-			parseLocationDirective(currentRoute, key, iss);
-		}
-	}
-
-	file.close();
+	currentServer = ServerConfig();
 	return true;
+}
+
+bool newLocationBlock(bool inLocationBlock, RouteConfig& currentRoute) {
+	if (inLocationBlock) {
+		std::cerr << "Nested location blocks are not allowed." << std::endl;
+		return false;
+	}
+	currentRoute = RouteConfig();
+	return true;
+}
+
+bool ConfigParser::parse(std::vector<ServerConfig>& servers){
+    std::ifstream file(filename.c_str());
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file: " << filename << std::endl;
+        return false;
+    }
+
+    std::string line;
+    ServerConfig currentServer;
+    RouteConfig currentRoute;
+    bool inServerBlock = false;
+    bool inLocationBlock = false;
+    int openBraces = 0;
+
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string key;
+        iss >> key;
+		if (line.find("{") != std::string::npos && line.find("}") == std::string::npos)
+			openBraces++;
+        if (key == "server") {
+            inServerBlock = newServerBlock(inServerBlock, currentServer);
+        } else if (key == "location") {
+            inLocationBlock = newLocationBlock(inLocationBlock, currentRoute);
+        } else if (key == "}") {
+            openBraces--;
+            if (inLocationBlock) {
+                currentServer.routes.push_back(currentRoute);
+                inLocationBlock = false;
+            } else if (inServerBlock) {
+                servers.push_back(currentServer);
+                inServerBlock = false;
+            }
+        } else {
+            if (inServerBlock && !inLocationBlock) {
+                parseServerDirective(currentServer, key, iss);
+            } else if (inServerBlock && inLocationBlock) {
+                parseLocationDirective(currentRoute, key, iss);
+            }
+        }
+    }
+
+    if (openBraces != 0) {
+        std::cerr << "Mismatched braces in configuration file." << std::endl;
+        return false;
+    }
+
+    file.close();
+    return true;
 }
 
 void ConfigParser::parseServerDirective(ServerConfig& server, const std::string& key, std::istringstream& iss) {
